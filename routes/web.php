ef4d76aad2bf9116dae2186dffcefc346cc7071c
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-01 10:51:55 +0900

    s

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get ( '/', function () {
 	Auth::routes();
 
 	Route::get ('/home', 'HomeController@index')->name('home');
+
+	Route::group(['namespace' => 'Api'], function () {
+		Route::post('/line/callback')->name('line.callback');
+	});
