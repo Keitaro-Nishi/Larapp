ce283872330878bf563632ba5e0374eca53d67a2
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-02-02 16:00:40 +0900

    s

--- app/Models/LineFriend.php
+++ app/Models/LineFriend.php
@@ -1,10 +1,15 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * Class LineFriend
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $line_id
+ * @property string $display_name
+ */
 class LineFriend extends Model
 {
-    //
+	protected $fillable = ['line_id', 'display_name'];
 }
